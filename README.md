1815b37de6bd51b51f7ca0bdd978e22eb5ecf6a5
edmilson <ecmelo.26@gmail.com>
2024-06-03 22:17:08 -0300

    alterando o Readme e refatorando a validacao nos campos cep latitude e longitude

--- README.md
+++ README.md
@@ -4,9 +4,21 @@
 
 Foco na acessibilidade e a simplicidade são fundamentais para o sucesso de qualquer sistema interno. Portanto, será criada uma interface de usuário atraente e funcional que permitirá aos funcionários inserir facilmente as informações necessárias para obter dados de endereço e previsões meteorológicas. 
 
+Este projeto visa criar uma ferramenta útil para o dia a dia dos usuários que trabalham com informações deste tipo, desenvolvendo telas focados no design e experiencia do usuário, fornecendo dados importantes sobre endereço e previsão do tempo através de uma API.
 
 
+## Ambiente de desenvolvimento
+> HTML, CSS, JavaScript e Insomnia
+
+
+#### Teste da requisição na API "viacep.com.br/" 
 ![](./img/apiCEP.png)
 
-
+#### Teste da requisição na API "open-meteo.com/en/docs/"
 ![](./img/apiPrevisao.png)
+
+Edmilson Correia Melo - [@LinkedIn](https://www.linkedin.com/in/edmilsonmelo2606/) - ecmelo.26@gmail.com
+
+Link do GitHub - [https://github.com/Edmilson2606/weather_forecast_and_location](https://github.com/Edmilson2606)
+
+Link do sistema disponibilizado - []()
--- js/script.js
+++ js/script.js
@@ -1,38 +1,64 @@
-// document.getElementById("first-name").addEventListener("change", validName);
-
-// document.getElementById("form").addEventListener("submit", function(event) {
-//     let firstName = document.getElementById("first-name");
-//     if (firstName.value.length < 3 || firstName.value.length === " ") {
-//         event.preventDefault();
-//         alert("O nome deve ter pelo menos 3 caracteres.");
-//         firstName = document.getElementById("first-name").focus();
-//     }
-// });
-
-function validName() {
-    let firstName = document.getElementById("first-name");
-    if (firstName.value.length < 3 || firstName.value.length === " "){
-        alert("O nome deve ter pelo menos 3 caracteres.");
-        firstName = document.getElementById("first-name").focus();
-    }    
-}    
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Tab' || e.which === 9) {
+        const inputsToValidate = ['cep', 'latitude', 'longitude'];
+        if (inputsToValidate.includes(e.target.id)) {
+            if (e.target.value === '') {
+                e.preventDefault();
+                let inputVar = e.target.id;
+                inputVar = inputVar.toUpperCase();
+                alert(`O campo ${inputVar} precisa ser informado.`);
+            }
+        }
+    }
+});
 
 function validEmail() {
-    let email = document.getElementById("email");
-    if (email.value.length === " ") {
-        alert("O email deve estar no formato correto.");
-        email = document.getElementById("email").focus();
+    if (email.value.length != "") {
+        let email = document.getElementById("email");
+        let emailTest = email.value;
+        const isValid = validateEmail(emailTest);
+        if(!isValid) {
+            alert("O email não estar no formato correto.");
+            email = document.getElementById("email").focus();
+        }
     }
 }
 
-function validCEP(){
-    let cep = document.getElementById("cep");
-    if (cep.value.length < 8) {
-        alert("O CEP deve estar no formato correto.");
-        cep = document.getElementById("cep").focus();
-    } 
+const validateEmail = (emailTest) => {
+    const regex = /^[^\s]+@[^\s]+\.[^\s]+$/;
+    return regex.test(emailTest);
 }
 
+// document.getElementById('cep').addEventListener('keydown', function(e) {
+//     if (e.key === 'Tab' || e.which === 9) {
+//         if (this.value === '') {
+//             e.preventDefault();
+//             alert("Precisa informar o CEP.");
+//             cep = document.getElementById("cep").focus();
+//         }
+//     }
+// });
+
+function validCEP(){
+    let cep = document.getElementById("cep");
+    let cepTest = cep.value;
+    cepTest = cepTest.replace(/\D/g, '');
+    if (cepTest === "") {
+        alert("Precisa informar o CEP.");
+        cep = document.getElementById("cep").focus();
+    } else {
+        const isValid = validateCEP(cepTest);
+        if (!isValid) {
+            alert("O CEP não estar no formato correto.");
+            cep = document.getElementById("cep").focus();
+        } 
+    }
+}
+
+const validateCEP = (cepTest) => {
+    const regex = /^[0-9]{8}$/;
+    return regex.test(cepTest);
+}
 
 async function getAddressByCep() {
     const cep = document.getElementById('cep').value;
@@ -40,36 +66,52 @@ async function getAddressByCep() {
         const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         const data = await response.json();
         console.log(data);
-        document.getElementById('rua').value = data.logradouro;
-        document.getElementById('bairro').value = data.bairro;
-        document.getElementById('cidade').value = `${data.localidade}/${data.uf}`;
+        if (data.logradouro != undefined) {
+            document.getElementById('rua').value = data.logradouro;
+            document.getElementById('bairro').value = data.bairro;
+            document.getElementById('cidade').value = `${data.localidade}/${data.uf}`;
+        } else {
+            document.getElementById('rua').value = 'CEP não encontrado.';
+            document.getElementById('bairro').value = '';
+            document.getElementById('cidade').value = '';
+        }
     } catch (error) {
         alert(error.message);
     }
 }
 
-async function getPrevisao() {
+// document.getElementById('latitude').addEventListener('keydown', function(e) {
+//     if (e.key === 'Tab' || e.which === 9) {
+//         if (this.value === '') {
+//             e.preventDefault();
+//             alert("Precisa informar a Latitude.");
+//             cep = document.getElementById("latitude").focus();
+//         }
+//     }
+// });
+
+// document.getElementById('longitude').addEventListener('keydown', function(e) {
+//     if (e.key === 'Tab' || e.which === 9) {
+//         if (this.value === '') {
+//             e.preventDefault();
+//             alert("Precisa informar a Longitude.");
+//             cep = document.getElementById("longitude").focus();
+//         }
+//     }
+// });
+
+async function getForecast() {
     const lat = document.getElementById('latitude').value;
     const lon = document.getElementById('longitude').value;
     try {
         const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,rain`);
         const data = await response.json();
-        console.log(data);
-        
+        console.log(data);  
         let date = new Date();
-        // let day = String(date.getDate()).padStart(2, '0');
-        // let month = String(date.getMonth() + 1).padStart(2, '0'); 
-        // let year = date.getFullYear()
         let hours = String(date.getHours()).padStart(2, '0');         
         hours = parseInt(hours);
-        // console.log(hours);
-
         let forecast = data.hourly.temperature_2m[hours]
         forecast = parseInt(forecast);
-
-        // document.getElementById('resposta').innerHTML = "";
-        // document.getElementById('resposta').innerHTML = `<div>${day}/${month}/${year} - ${hours}h - ${data.hourly.temperature_2m[hours]}° C</div>`;
-
         document.getElementById('forecast').innerHTML = `${forecast}° C`;
     } catch (error) {
         alert(error.message);
